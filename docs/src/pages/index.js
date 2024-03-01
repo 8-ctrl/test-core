77a32b177fced3f41c493a6b2abb5ddf47875720
8-ctrl <neelpatel1029@gmail.com>
2024-03-01 18:32:41 +0530

    wip: test

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   }, []);
 
   return (
-    <Layout title={`Zelthy Docs test 1234 test xyz`} description="Zelthy Docs">
+    <Layout title={`Zelthy Docs test 1234 test xy`} description="Zelthy Docs">
       <main>
         {/* <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
         <img src={ZelthyLandingPageBgImg} />
